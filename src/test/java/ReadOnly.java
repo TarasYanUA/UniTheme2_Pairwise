7e988443d71b8dcd5a16e5d113b8d768abe2a6b0
TarasYanUA <wildmonach@gmail.com>
2023-12-18 11:39:44 +0200

    Cycle to read the whole excel file is ready!

--- src/test/java/ReadOnly.java
+++ src/test/java/ReadOnly.java
@@ -1,53 +1,24 @@
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellType;
 import org.testng.annotations.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
 
 public class ReadOnly {
     @Test
     public static void readFromExcel() throws IOException {
         HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("Second test.xlsx"));
-        HSSFSheet myExcelSheet = myExcelBook.getSheet("Sheet1");
-        //HSSFRow row = myExcelSheet.getRow(1);
+        HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
 
-        // Цикл по строкам
-        for (int i = 1; i < 12; i++) {
-            HSSFRow row = myExcelSheet.getRow(i);
-            System.out.println(row);
+        for (int k = 1; k <= 11; k++) {
+            HSSFRow row = myExcelSheet.getRow(k);
+            for (int i = 0; i < 8; i++) {
+                String stringOfRow = row.getCell(i).getStringCellValue();
+                System.out.println(stringOfRow);
+            }
         }
         System.out.println(" ");
-
         myExcelBook.close();
-
-
-           /* // Перебрать каждый столбец в выбранной строке
-            for (int j = 0; j < cols; j++) {
-                // Значение ячейки Pring
-                System.out.print(worksheet.getCells().get(i, j).getValue() + " | ");
-            }
-            // Распечатать разрыв строки*/
-
-
-
-
-
-
-
-/*        if(row.getCell(0).getCellType() == CellType.STRING){
-            String name = row.getCell(0).getStringCellValue();
-            System.out.println("name : " + name);
-        }
-
-        if(row.getCell(1).getCellType() == CellType.NUMERIC){
-            Date birthdate = row.getCell(1).getDateCellValue();
-            System.out.println("birthdate :" + birthdate);
-        }*/
-
-
     }
 }
