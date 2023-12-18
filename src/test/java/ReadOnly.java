5f966e29c2532a3382ad0d15bc2f3a6cb70859dd
TarasYanUA <wildmonach@gmail.com>
2023-12-18 16:33:02 +0200

    Added automatic count of excel columns.

--- src/test/java/ReadOnly.java
+++ src/test/java/ReadOnly.java
@@ -11,12 +11,16 @@ public class ReadOnly {
         HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream("Second test.xlsx"));
         HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
 
-        int rowNumbers = myExcelSheet.getPhysicalNumberOfRows();
-        String[][] testCases = new String[rowNumbers-1][8];
+        int rowNumber = myExcelSheet.getPhysicalNumberOfRows();
+        int columnNumber = myExcelSheet.getRow(0).getPhysicalNumberOfCells();
+        System.out.println("Number of rows: " + rowNumber);
+        System.out.println("Number of columns: " + columnNumber);
 
-        for (int k = 1; k < rowNumbers; k++) {
+        String[][] testCases = new String[rowNumber-1][columnNumber];
+
+        for (int k = 1; k < rowNumber; k++) {
             HSSFRow row = myExcelSheet.getRow(k);
-                for (int i = 0; i < 8; i++) {
+                for (int i = 0; i < columnNumber; i++) {
                     String stringOfRow = row.getCell(i).getStringCellValue();
                     testCases[k-1][i] = stringOfRow;
             }
