245a42a9e9d6caeed403c557923da56cc58e026c
TarasYanUA <wildmonach@gmail.com>
2023-12-19 12:51:06 +0200

    First settings and its conditions are ready and work successfully.

--- src/main/java/ColorScheme.java
+++ src/main/java/ColorScheme.java
@@ -54,11 +54,95 @@ public class ColorScheme extends CsCart {
         select_General_RoundCornersForInterfaceElements.selectOptionByValue("full");
     }
 
+    SelenideElement checkboxDisplayHeadersInCapitalLetters = $(By.id("settings.abt__ut2.general.use_titles_uppercase"));
+    public void ColSch_DHICL_On(){
+        condition_ColorSchemePage_tabGeneral();
+        if(!checkboxDisplayHeadersInCapitalLetters.isSelected()){
+            checkboxDisplayHeadersInCapitalLetters.click();
+        }
+    }
+    public void ColSch_DHICL_Off(){
+        condition_ColorSchemePage_tabGeneral();
+        if(checkboxDisplayHeadersInCapitalLetters.isSelected()){
+            checkboxDisplayHeadersInCapitalLetters.click();
+        }
+    }
+
+    SelenideElement checkboxDisplayTextInCapitalLetters = $(By.id("settings.abt__ut2.general.buttons.use_text_uppercase"));
+    public void ColSch_DTICL_On(){
+        condition_ColorSchemePage_tabGeneral();
+        if(!checkboxDisplayTextInCapitalLetters.isSelected()){
+            checkboxDisplayTextInCapitalLetters.click();
+        }
+    }
+    public void ColSch_DTICL_Off(){
+        condition_ColorSchemePage_tabGeneral();
+        if(checkboxDisplayTextInCapitalLetters.isSelected()){
+            checkboxDisplayTextInCapitalLetters.click();
+        }
+    }
+
 
     //Вкладка "Списки товаров"
     SelenideElement select_General_FrameTypeOfProduct = $(By.id("settings.abt__ut2.product_list.show_grid_border"));
+    public void ColSch_FTPG_Not(){
+        condition_ColorSchemePage_tabProductLists();
+        select_General_FrameTypeOfProduct.selectOptionByValue("none");
+    }
     public void ColSch_FTPG_Without(){
         condition_ColorSchemePage_tabProductLists();
         select_General_FrameTypeOfProduct.selectOptionByValue("solid_without_margins");
+    }
+    public void ColSch_FTPG_With(){
+        condition_ColorSchemePage_tabProductLists();
+        select_General_FrameTypeOfProduct.selectOptionByValue("solid_with_margins");
+    }
+
+    SelenideElement checkboxAddBackgroundForProductImages = $(By.id("settings.abt__ut2.product_list.mask_images_gallery"));
+    public void ColSch_ABMFPI_On(){
+        condition_ColorSchemePage_tabProductLists();
+        if(!checkboxAddBackgroundForProductImages.isSelected()){
+            checkboxAddBackgroundForProductImages.click();
+        }
+    }
+    public void ColSch_ABMFPI_Off(){
+        condition_ColorSchemePage_tabProductLists();
+        if(checkboxAddBackgroundForProductImages.isSelected()){
+            checkboxAddBackgroundForProductImages.click();
+        }
+    }
+
+    SelenideElement selectUseElementsAlignment = $(By.id("settings.abt__ut2.product_list.use_elements_alignment"));
+    public void ColSch_UEA_Use(){
+        condition_ColorSchemePage_tabProductLists();
+        selectUseElementsAlignment.selectOptionByValue("use");
+    }
+    public void ColSch_UEA_Dont(){
+        condition_ColorSchemePage_tabProductLists();
+        selectUseElementsAlignment.selectOptionByValue("do_not_use");
+    }
+
+    SelenideElement checkboxExpandGridItemOnHover = $(By.id("settings.abt__ut2.product_list.extend_grid_item_on_hover"));
+    public void ColSch_EGIOH_On(){
+        condition_ColorSchemePage_tabProductLists();
+        if(!checkboxExpandGridItemOnHover.isSelected()){
+            checkboxExpandGridItemOnHover.click();
+        }
+    }
+    public void ColSch_EGIOH_Off(){
+        condition_ColorSchemePage_tabProductLists();
+        if(checkboxExpandGridItemOnHover.isSelected()){
+            checkboxExpandGridItemOnHover.click();
+        }
+    }
+
+    SelenideElement selectFontWeightForProductName = $(By.id("settings.abt__ut2.product_list.grid-list.product_name_font_weight"));
+    public void ColSch_FWFPN_Normal(){
+        condition_ColorSchemePage_tabProductLists();
+        selectFontWeightForProductName.selectOptionByValue("normal");
+    }
+    public void ColSch_FWFPN_Bold(){
+        condition_ColorSchemePage_tabProductLists();
+        selectFontWeightForProductName.selectOptionByValue("bold");
     }
 }
--- src/test/java/TestCase_01_Test.java
+++ src/test/java/TestCase_01_Test.java
@@ -5,8 +5,14 @@ public class TestCase_01_Test extends TestRunner {
     public void checkTestCase_01(){
         CsCart csCart = new CsCart();
         ColorScheme colorScheme = new ColorScheme();
-        colorScheme.ColSch_RCFIE_Dont();
+        colorScheme.ColSch_RCFIE_Little();
+        colorScheme.ColSch_DHICL_Off();
+        colorScheme.ColSch_DTICL_Off();
         colorScheme.ColSch_FTPG_Without();
+        colorScheme.ColSch_ABMFPI_Off();
+        colorScheme.ColSch_UEA_Use();
+        colorScheme.ColSch_EGIOH_On();
+        colorScheme.ColSch_FWFPN_Bold();
         csCart.button_Save.click();
     }
 }
